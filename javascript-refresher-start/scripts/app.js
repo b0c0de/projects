7a10d2c98de2c6b058e907600d8c16409ae6ea1b
Bojan Cvjetković <39283490+b0c0de@users.noreply.github.com>
2023-10-28 22:59:34 +0200

    Update app.js
    Commenting out

--- javascript-refresher-start/scripts/app.js
+++ javascript-refresher-start/scripts/app.js
@@ -22,27 +22,28 @@
   //return userName + message;
 //}
 
-const user = {
-  name: 'Max',
-  age: 34
-  greet () {
-    console.log("Hello!");
-    console.log(this.age);
-  }
-};
+//const user = {
+ // name: 'Max',
+  //age: 34
+  //greet () {
+   // console.log("Hello!");
+    //console.log(this.age);
+ // }
+//};
 
-console.log(user.name);
-user.greet();
+//console.log(user.name);
+//user.greet();
 
-class User {
-  constructor(name, age) {
-    this.name = name; 
-    this.age = age;
-  }
-  greet() {
-    console.log('Hi'); 
-  }
-}
+//class User {
+  //constructor(name, age) {
+    //this.name = name; 
+    //this.age = age;
+ // }
+  //greet() {
+   // console.log('Hi'); 
+  //}
+//}
 
 const user1 = new User("Manuel", 35);
 console.log(user1);
+user1.greet();
