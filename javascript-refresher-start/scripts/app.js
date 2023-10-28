a764bc6235568693d9a8dfbb9eaad92e9c2faac8
Bojan Cvjetković <39283490+b0c0de@users.noreply.github.com>
2023-10-28 22:23:06 +0200

    Update app.js
    Arrow function

--- javascript-refresher-start/scripts/app.js
+++ javascript-refresher-start/scripts/app.js
@@ -6,14 +6,17 @@
 //console.log("Hello"+"world");
 //console.log(userMessage);
 //console.log (10 === 10);
-function createGreeting(userName, message = "Hello") {
+//function createGreeting(userName, message = "Hello") {
   //console.log(userName);
   //console.log(message);
-  return "Hi, I am " + userName + "." + message';
+//  return "Hi, I am " + userName + "." + message';
+//}
+
+//const greeting1 = createGreeting ("Max");
+//console.log(greeting1);
+
+//const greeting2 = createGreetng ("Manuel", "Hello, what's up?");
+//console.log(greeting2);
+export default function () {
+  console.log('Hello');
 }
-
-const greeting1 = createGreeting ("Max");
-console.log(greeting1);
-
-const greeting2 = createGreetng ("Manuel", "Hello, what's up?");
-console.log(greeting2);
