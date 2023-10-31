cab3a03a06baa168ba4df40511597f03f456a3d6
Bojan Cvjetković <39283490+b0c0de@users.noreply.github.com>
2023-10-31 23:37:00 +0100

    Update app.js
    Function inside of a function

--- javascript-refresher-start/scripts/app.js
+++ javascript-refresher-start/scripts/app.js
@@ -119,18 +119,27 @@
 //const list = document.querySelector ("ul");
 //list.remove();
 
-function handleTimeout() {
-  console.log("Timed out");
+//function handleTimeout() {
+//  console.log("Timed out");
+//}
+//cons handleTimeout2 = () => {
+//  console.log("Timed out ... again!");
+//};  
+
+//setTimeout(handleTimeout, 2000);
+
+
+//function greeter (greetFn) {
+//  greetFn();
+//}
+
+//greeter (() => console.log("Hi"));
+
+function init() {
+  function greet () {
+    console.log("Hi"); 
+  }
+  greet();
 }
-cons handleTimeout2 = () => {
-  console.log("Timed out ... again!");
-};  
 
-setTimeout(handleTimeout, 2000);
-
-
-function greeter (greetFn) {
-  greetFn();
-}
-
-greeter (() => console.log("Hi"));
+greet(); 
